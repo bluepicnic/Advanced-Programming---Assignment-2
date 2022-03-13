4656685c3babd0357eb6fd05e68a7e7afd0d1f4b
Cameron Yorke <cameronyorkecy@hotmail.com>
2022-03-13 14:23:55 +0000

    Added comments to File Parser class and member functions

--- Headers/fileParser.h
+++ Headers/fileParser.h
@@ -1,6 +1,5 @@
-#pragma once
+//only include file once
+#pragma once 
 
-
-//INSERT FILE PARSING FUNCTIONS HERE
-void parseFile(Board boards[], vector<Ship> &ships);
-vector<string> extractFileParameters(string toExtract, char delimiter);
+void parseFile(Board boards[], vector<Ship> &ships); //analyze values from adaship_config.ini
+vector<string> extractFileParameters(string toExtract, char delimiter); //separate values from each line of the config file based on a supplied delimiter
--- Source/fileParser.cpp
+++ Source/fileParser.cpp
@@ -1,13 +1,11 @@
 #include "../Headers/headers.h"
 
-//File parse functions here
 //Check and apply contents of file 
 
 //We want to use this function once per created game and ensure both players recieve identical game setup data
 
 void parseFile(Board boards[], vector<Ship> &ships) 
 {   
-  //usual file setup, check if exists
   string line = "";
   string type = "";
   int boardWidth = 0;
@@ -20,11 +18,11 @@ void parseFile(Board boards[], vector<Ship> &ships)
     cout << "Config file does not exist";
   } 
   else {
-    while (!configFile.eof()) { //obtain all config information within file
-      getline(configFile, type, ':'); 
+    while (!configFile.eof()) { //obtain game configuration info line by line
+      getline(configFile, type, ':'); //Each line starts the type of setup information it is providing
       type = convertToLower(type); //ignore case 
       
-      if (type == "board")
+      if (type == "board") //if its the first line of the file, setup type will be board
       {
         char delim = 'a'; //can't initialise a char with a blank character??
         if(line.find('x')) { //look for a lower case 
@@ -33,14 +31,14 @@ void parseFile(Board boards[], vector<Ship> &ships)
         else { //just in case the board has an upper case x
           delim = 'X';
         }
-        getline(configFile, line);
+        getline(configFile, line); //get remainder of line, we'll separate parameters ourselves
         
         parameters = extractFileParameters(line, delim);
         
         boardWidth = stoi(removeLeadTrailSpaces(parameters[0]));
         boardHeight = stoi(removeLeadTrailSpaces(parameters[1]));
 
-        Board help (boardWidth, boardHeight); 
+        Board help (boardWidth, boardHeight); //temporary board to set both player boards with
 
         for (int i = 0; i < num_Boards; i++) {
           boards[i] = help;
@@ -49,10 +47,11 @@ void parseFile(Board boards[], vector<Ship> &ships)
       }
       
       if (type == "boat") {
+        //get entire line, point of separation in a boat config string of text will always be a comma
         getline(configFile, line);
         parameters = extractFileParameters(line, ',');
 
-        
+        //prepare extracted ship elements for a temporary ship, which we'll push back into a player's boat vector
         string shipName = removeLeadTrailSpaces(parameters[0]);
         int sizeHealth = stoi(removeLeadTrailSpaces(parameters[1]));
         Ship tmpShip(shipName, sizeHealth, index);
@@ -62,39 +61,16 @@ void parseFile(Board boards[], vector<Ship> &ships)
       }
     }
   }
-
-  //while not EOF
-   //create a ship object
-   
-   
-  //if first line (split.left = board)
-   //read first line, grab width and height
-   //create board object (pass in width and height)
-   //loop per number of boards to setup both boards using constructors 
-
-
-  //if boat lines (split.left = ship)
-  //setup an individual ship
-  //push back into vector 
-   
-   
-   
-
 }
-
-//game class 
-
-//either data is extracted and returned
 
 vector<string> extractFileParameters(string toExtract, char delimiter)
 {
-  vector <string> params; //there could be a variable number of parameters to extract 
-  std::stringstream strStream(toExtract); //need a stringstream similar to a file to use getline on
+  vector <string> params; //there could be a variable number of elements to extract 
+  stringstream strStream(toExtract); //use an individual line of a file to use as a stringstream
   string temp = "";
-  while (getline(strStream, temp, delimiter)) //even if the delimiter is not found, the loop will continue for the remainder of the stringstream, allowing us to return multiple distinct string objects
-  {
+  while (getline(strStream, temp, delimiter)) {
+    //even if the delimiter is not found, the loop will continue for the remainder of the stringstream, allowing us to return multiple distinct string objects
     params.push_back(temp); //store separated string items to return
   }
-
   return params;
 }
